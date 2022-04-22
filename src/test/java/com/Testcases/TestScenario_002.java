6304013b8f2ddbe295aa7b39258d435e52643dec
venkatprithvi <70633305+venkatprithvi@users.noreply.github.com>
2022-04-22 23:33:44 +0530

    22-4-22_11:33PM

---
+++ src/test/java/com/Testcases/TestScenario_002.java
@@ -0,0 +1,137 @@
+package com.Testcases;
+
+import java.io.IOException;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.ObjectRepository.Locators;
+import com.Utilities.Wrapper;
+
+public class TestScenario_002 extends Wrapper {
+	Locators loc = new Locators();
+
+	/*********************************************************************
+	 * Before function to run driver creation and loading properties file
+	 * 
+	 * @throws IOException
+	 * @throws InterruptedException 
+	 *********************************************************************/
+	@BeforeMethod
+	public void BeforeTestcaseRun() throws IOException, InterruptedException {
+		Thread.sleep(2000);
+		loaddata_From_Prop_File("Z:\\SeleniumEclipseWorkspace\\JRI_PROJECT\\testData\\input.properties");
+		multiBrowserLaunch("Chrome");
+	}
+
+	/*********************************************************************
+	 * After function to run driver creation and loading properties file
+	 * @throws Exception 
+	 *********************************************************************/
+	@AfterMethod
+	public void AfterTestcaseRun() throws Exception {
+		implicitWait(10);
+		browserClosing();
+	}
+
+	/************************************************************
+	 * TC 011 : Test case to check project home page is opening or not
+	 **************************************************************/
+	@Test
+	public void Tc_011() throws Exception {
+		UrlPageOpening(getdata_from_prop("HomePageURL"));
+		Thread.sleep(3000);
+		Assert.assertEquals("https://www.justrechargeit.com/", driver.getCurrentUrl());
+	}
+	
+	/************************************************************
+	 * TC 012 : sign-in page sign-in link test
+	 **************************************************************/
+	@Test
+	public void Tc_012() throws Exception {
+		Tc_011();
+		clickByLocator(loc.sign_in_a_href_link);
+		clickByLocator(loc.sign_in_inside_sign_in);
+		Assert.assertEquals("https://www.justrechargeit.com/SignIn.aspx", driver.getCurrentUrl());
+	}
+	
+	/************************************************************
+	 * TC 013 : sign-in page sign-in link test
+	 **************************************************************/
+	@Test
+	public void Tc_013() throws Exception {
+		Tc_011();
+		clickByLocator(loc.sign_in_a_href_link);
+		clickByLocator(loc.sign_in_inside_sign_in);
+		if(
+		driver.findElement(loc.Sign_in_email_field).isDisplayed()
+		&&
+		driver.findElement(loc.Sign_in_password_field).isDisplayed()
+		&&
+		driver.findElement(loc.Sign_in_capture_field).isDisplayed()
+		&&
+		driver.findElement(loc.Sign_in_secure_sign_in_buttion).isDisplayed()
+		&&
+		driver.findElement(loc.Sign_in_sign_in_with_facebook).isDisplayed()
+		&&
+		driver.findElement(loc.Sign_in_sign_in_with_google).isDisplayed()
+		) 
+		{
+			System.out.println("all web elements founded ");
+		}
+		else 
+		{
+			System.out.println("all elements not found");
+		}
+		
+		clickByLocator(loc.Sign_in_forgot_password);
+		if(
+			driver.findElement(loc.Sign_in_forgot_password_emailid).isDisplayed()
+			&&
+			driver.findElement(loc.Sign_in_forgot_password_get_password).isDisplayed()
+			) 
+		{
+			System.out.println("web elements displayed");
+		}else {
+			System.out.println("web elements not found");
+		}
+	}
+
+	/************************************************************
+	 * TC 014 : sign-in page sign-in link test
+	 **************************************************************/
+	@Test
+	public void Tc_014() throws Exception {
+		Tc_012();
+		clickByLocator(loc.Sign_in_secure_sign_in_buttion);
+		Assert.assertEquals("Enter your email", getTextByAnyLocator(loc.sign_in_email_validation));
+		sendKeysByAnyLocator(loc.Sign_in_email_field, "venkatprithvi.p@gmail.com");
+		clickByLocator(loc.Sign_in_secure_sign_in_buttion);
+		Assert.assertEquals("Enter your password", getTextByAnyLocator(loc.sign_in_password_validation));
+		sendKeysByAnyLocator(loc.Sign_in_email_field, "venkatprithvi.p@gmail.com");
+		sendKeysByAnyLocator(loc.Sign_in_password_field, "ap31dd6406");
+		clickByLocator(loc.Sign_in_secure_sign_in_buttion);
+		Assert.assertEquals("Enter captcha properly", getTextByAnyLocator(loc.sign_in_capture_validation));
+	}
+	
+	/************************************************************
+	 * TC 015 : sign-in page sign-in link test
+	 **************************************************************/
+	@Test
+	public void Tc_015() throws Exception {
+		Tc_012();
+		sendKeysByAnyLocator(loc.Sign_in_email_field, "venkatprithvi.p@gmail.com");
+		sendKeysByAnyLocator(loc.Sign_in_password_field, "ap31dd6406");
+		clickByLocator(loc.Sign_in_secure_sign_in_buttion);
+		Assert.assertEquals("Enter captcha properly", getTextByAnyLocator(loc.sign_in_capture_validation));
+	}
+	
+	public void Tc_17() throws Exception {
+		Tc_012();
+		clickByLocator(loc.Sign_in_forgot_password);
+		
+	}
+	
+}
